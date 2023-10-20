78a30f774e355605f7e011021bf814b8acfaaa6a
cristian <cristian.mazzarone@edu.unifi.it>
2023-10-20 17:43:01 +0200

    Html e css del main menu

--- src/app/components/main-menu/main-menu.component.ts
+++ src/app/components/main-menu/main-menu.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
-  styleUrls: ['./main-menu.component.scss']
+  styleUrls: ['./main-menu.component.scss'],
 })
 export class MainMenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  setDifficulty(arg0: string) {
+    throw new Error('Method not implemented.');
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
